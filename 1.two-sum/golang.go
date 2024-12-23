81588670e23270a4938df80cce59659a69f3fb5c
Mondo <mondo.jiang@wisc.edu>
2024-12-23 17:48:37 -0600

    344 go

---
+++ 1.two-sum/golang.go
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+package twosum
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	arr := make([]int, 2)
+	return arr
+}
+// @lc code=end
+
---
+++ 590.n-ary-tree-postorder-traversal/golang.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=590 lang=golang
+ *
+ * [590] N-ary Tree Postorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func postorder(root *Node) []int {
+    
+}
+// @lc code=end
+
