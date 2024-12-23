51d55e031b678691c9eeb5f5fd251c978a97148d
Mondo <mondo.jiang@wisc.edu>
2024-12-23 17:53:19 -0600

    344 go

---
+++ 344.reverse-string/golang-1.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=344 lang=golang
+ *
+ * [344] Reverse String
+ */
+package reversestring
+
+// @lc code=start
+func reverseString(s []byte) {
+	var (
+		len  = len(s)
+		stop = len / 2
+		head int
+		tail int = len - 1
+	)
+
+	for ; head < stop; head, tail = head+1, tail-1 {
+		temp := s[tail]
+		s[tail] = s[head]
+		s[head] = temp
+	}
+}
+
+// @lc code=end
--- 344.reverse-string/golang-2.go
+++ 344.reverse-string/golang-2.go
@@ -3,20 +3,16 @@
  *
  * [344] Reverse String
  */
-package reversestring
-
-import "fmt"
+package reversestring2
 
 // @lc code=start
 func reverseString(s []byte) {
 	var (
-		len  = len(s)
-		stop = len / 2
 		head int
-		tail int = len - 1
+		tail int = len(s) - 1
 	)
 
-	for ; head < stop; head, tail = head+1, tail-1 {
+	for ; head < tail; head, tail = head+1, tail-1 {
 		temp := s[tail]
 		s[tail] = s[head]
 		s[head] = temp
