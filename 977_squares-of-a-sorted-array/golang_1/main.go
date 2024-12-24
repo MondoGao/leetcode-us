7e0552a4302905442085effa27a246f097a4170a
Mondo <mondo.jiang@wisc.edu>
2024-12-23 20:52:02 -0600

    977 go

--- 977_squares-of-a-sorted-array/golang_1/main.go
+++ 977_squares-of-a-sorted-array/golang_1/main.go
@@ -5,9 +5,34 @@
  */
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // @lc code=start
 func sortedSquares(nums []int) []int {
-    
-}
-// @lc code=end
+	var (
+		in_len     = len(nums)
+		out        = make([]int, in_len)
+		head, tail = 0, in_len - 1
+		cur        = in_len
+	)
 
+	for head <= tail {
+		cur--
+		head_v := float64(nums[head])
+		tail_v := float64(nums[tail])
+		if math.Abs(float64(head_v)) > math.Abs(float64(tail_v)) {
+			out[cur] = int(math.Pow(head_v, 2))
+			head++
+			continue
+		}
+
+		out[cur] = int(math.Pow(tail_v, 2))
+		tail--
+	}
+	return out
+}
+
+// @lc code=end
